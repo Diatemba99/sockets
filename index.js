ab3278756e89545ed24aa739ab52cf75b6a47c45
Shellghoste <shellghoste@gmail.com>
2022-03-04 18:40:17 +0000

    adding disconnection

--- index.js
+++ index.js
@@ -9,15 +9,22 @@ app.get('/', (req, res )=> {
     res.sendFile(`${__dirname}/public/index.html`)
 });
 
+
+//Connection code 
 io.on('connection', (socket) => {
     console.log("USer connected !");
     socket.on('chat message', (msg) => {
         //console.log("Message: " + msg);
         io.emit("chat message", msg);
     });
-
     
 });
+
+//Disconnect code
+io.on('disconnect', () => {
+    console.log("USer disconnected !");
+
+    });
 
 
 server.listen(3000, () => {
